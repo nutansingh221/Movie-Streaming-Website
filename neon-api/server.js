736aec61d3ab52b8a01ee2f054b9c1814461d00f
Nutan <nutansingh7696@gmail.com>
2023-01-11 14:43:47 +0530

    first commit

---
+++ neon-api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/UserRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/neonx", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("DB connected");
+});
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, console.log("server started on port 5000"));
