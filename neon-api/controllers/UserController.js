147832e9b70b93e07c52487aac641a7564b17ba2
Nutan <nutansingh7696@gmail.com>
2023-01-18 17:06:36 +0530

    update

--- neon-api/controllers/UserController.js
+++ neon-api/controllers/UserController.js
@@ -23,3 +23,38 @@ module.exports.addToLikedMovies = async (req, res) => {
         return res.json({msg: "Error adding movies"});
     }
 };
+
+module.exports.getLikedMovies = async(req,res) => {
+    try{
+        const { email } = req.params;
+        const user = await User.findOne({ email });
+        if(user) {
+            return res.json({msg: "sucess", movies: user.likedMovies });
+        }else return res.json({msg: "user with given email not found."})
+    } catch(err){
+        return res.json({msg: "Error fetching movie"});
+    }
+}
+
+module.exports.removeFromLikedMovies = async(req,res) => {
+    try{
+        const { email,movieId } = req.body;
+        const user = await User.findOne({ email });
+        if(user){
+            const { likedMovies } = user;
+            const movieIndex = likedMovies.findIndex(({ id }) => (id === movieId));
+            if(!movieIndex) res.status(400).send({msg:"Movie not found"});
+            likedMovies.splice(movieIndex,1);
+            await User.findByIdAndUpdate(
+                user._id,
+                {
+                    likedMovies,
+                },
+                { new: true }
+            );
+            return res.json({ msg: "Movie Deleted", movies: likedMovies }); 
+        }
+    }catch(err){
+        return res.json({msg: "Error deleting movie"}); 
+    }
+}
